cfde89ff9472b32be32757a50767cfff37a82edd
Gabriel Divtech <gabriel.lira@divtech.com.br>
2023-12-15 10:05:29 -0300

    Implementando os desafios solicitados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("83999877876", "Modelo beta", "11111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Smartphone iphone:");
+Smartphone iphone = new Iphone("8199001010", "Modelo teste", "33333333333333333", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Tiktok");
